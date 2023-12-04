5800dd41edb14c0b99540dbdceaa80a4b00e343e
Alexandra Li <alexandrali766@gmail.com>
2023-12-04 11:03:40 -0500

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# simulation
+# Simulation
+
+The code needed to run the simulation exists in `run.py`.
+
+## Choice Model Parameters
+
+The parameters for different choice models can be read in with the $\texttt{parse\_params}$ function. The function takes in the file name and returns a dictionary of model names as keys with a dictionary of parameters as values. 
+
+## Running the Code
+The function $\texttt{run\_and\_plot}$ takes in the different tunable parameters for the simulation, including the ratio of the choice model parameters excluding the booking constant, the ratio of the booking constant, and other factors. It plots the acceptance probabilities and average number of accepted loads over 5 runs of each pricing algorithm. 
--- run.py
+++ run.py
@@ -4,14 +4,14 @@ xcel_params = parse_params("logit_interactions.xlsx")
 # for p in xcel_params.keys():
 #   print(p, xcel_params[p])
 
-pricing_algs = ["zero_all", "uber_low", "uber_avg", "uber_high", "max_all", "dp_alg 2"]
-
-r = [1]
-booking_ratio = [1] * len(r)
-carriers_to_loads_ratio = 1.5
 bundle_generation_options = ["uber"]
+pricing_algs = ["zero_all", "uber_low", "uber_avg", "uber_high", "max_all", "dp_alg 2"]
 num_loads = 100
+carriers_to_loads_ratio = 1.5
 model_params = xcel_params["Model 1"]
 
+params_ratio = [1]
+booking_ratio = [1]
+
 run_and_plot(bundle_generation_options, pricing_algs, num_loads, carriers_to_loads_ratio, model_params, 
-                booking_ratio = booking_ratio, ratio = r, penalty_ratio = 2)
+                booking_ratio = booking_ratio, ratio = params_ratio, penalty_ratio = 2)
